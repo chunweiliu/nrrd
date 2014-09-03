090dcf13276befe4e6b1c5f1b6ef9345f60ba698
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-09-02 22:29:14 -0400

    MATLAB index starts at 1

--- ijk2ras.m
+++ ijk2ras.m
@@ -10,7 +10,8 @@ D = [C{1} C{2} C{3}];
 C = textscan(meta.spaceorigin,'(%f,%f,%f)'); % original in LPS
 d = [-(s(1)*D(1,1)+C{1}) -(s(2)*D(2,2)+C{2}) C{3}]'; % original in RAS
 
-% transfrom ras 
+% transfrom ras
+ijk = ijk - 1; % MATLAB start at index 1
 tmp = [s(2)-ijk(2) s(1)-ijk(1) ijk(3)]';
 ras = D*tmp+d;
 
